6cf85f0dedf7630b24e44bb86f96f528146d9761
vinicius cardoso <cardosovinicius87@gmail.com>
2022-11-10 18:15:02 -0300

    feat: desafio 15

---
+++ challenges/desafio15.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
+db.produtos.updateMany({ tags: { $in: ["bovino"] } }, { $set: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: { $in: ["ave"] } }, { $set: { avaliacao: 3 } });
+
+db.produtos.find({ }, { _id: 0, nome: 1, avaliacao: 1 });

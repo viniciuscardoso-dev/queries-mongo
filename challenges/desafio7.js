c5575842ae32058fd8c62cd918aa0a5fdc432b18
vinicius cardoso <cardosovinicius87@gmail.com>
2022-11-10 11:16:56 -0300

    fix: erros de lint

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,4 +1,4 @@
 db.produtos.find({ $and: [
   { vendidos: { $ne: 50 } },
-  { tags: { $exists: false } }
-] }, {_id: 0, nome: 1, vendidos: 1});
+  { tags: { $exists: false } },
+] }, { _id: 0, nome: 1, vendidos: 1 });
